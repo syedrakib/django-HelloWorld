510c89552e4cc59dd79a3cae71b1df15d71e125c
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-02-22 17:28:58 +0600

    adding the Choice model into the admin panel So far, only the Question model was being addressed in the admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question , Choice
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -10,3 +11,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
