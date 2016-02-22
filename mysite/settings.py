73fa118ea872f04e15a4d5d7562a17908de2502d
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-02-22 16:39:49 +0600

    setup MySQL Database connection settings set local timezone

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE':    'django.db.backends.mysql',
+       'NAME':      'hello_django',
+       'USER':      'root',
+       'PASSWORD':  'root',
+       'HOST':      '127.0.0.1',
+       'PORT':      '3306'
     }
 }
 
@@ -87,7 +91,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
