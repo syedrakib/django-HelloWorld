efaac03bc8002c5aea75355157882c3a2a208e16
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-02-23 17:39:06 +0600

    write more views for more URLs of the polls application

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote),
 ]
