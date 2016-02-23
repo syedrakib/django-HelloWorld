bff3db801612b2258114a3b6a548e4767b8fff98
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-02-24 03:42:39 +0600

    Add HTML form to the "details" template for voting on a poll. Redirect to "results" page upon successful voting.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from django.http import Http404
-from .models import Question
+from .models import Question , Choice
 
 # Create your views here.
 def index(request):
@@ -23,5 +23,21 @@ def results(request , question_id):
     return HttpResponse(response % question_id)	
 
 def vote(request , question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    q = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = q.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        context = {
+            'question': q,
+            'error_message': "You didn't select a choice."
+        }
+        return render(request , 'polls/details.html' , context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect('/polls/%s/results' % q.id)
 
