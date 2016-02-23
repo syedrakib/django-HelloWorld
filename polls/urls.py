16c916737952114eb52fcfb981a3be9372929205
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-02-24 05:33:48 +0600

    use generic views provided by django instead of having to write our own view codes

--- polls/urls.py
+++ polls/urls.py
@@ -4,11 +4,15 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    # Generic views normally expect the primary key value [captured from the URL] to be called "pk".
+    # So we’ve changed the "question_id" variable name to "pk" for using with the generic views.
+
 ]
